76c57434c821b31a97bfa8b58905651d06ae5c74
Hawkyne <adnanh.sazin@gmail.com>
2024-01-20 22:26:10 +0600

    Update Funoken.sol

--- contracts/Funoken.sol
+++ contracts/Funoken.sol
@@ -1,82 +1,109 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Funoken is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    
+    // Errors
+    error NotForSale();
+    error NotOwner();
+    error AuctionActive();
+    error AuctionTimeExpired();
+    error OwnerNotAllowed();
+    error ContractNotAllowed();
+    error BidTooLow();
+
     struct TokenBid{
-        bool forSale;
         uint256 highestBid;
-        address highestBidder;
         uint256 auctionStart;
-        uint8 bidsPlaced;
-        uint8 activeTime;
+        uint256 bidsPlaced;
+        uint256 activeTime;
+        address highestBidder;
+        bool forSale;
     }
+
+    uint256 private _tokenId;
     
     mapping (uint256 => TokenBid) public idToBid;
+
     event Bid(address bidder, uint256 tokenId, uint256 amount);
     event Auction(uint256 tokenId, address currentOwner);
     event Transaction(uint256 tokenId, address from, address to, uint256 amount);
 
     modifier upForSale (uint256 _itemId) {
-        require(idToBid[_itemId].forSale);
+        if(!idToBid[_itemId].forSale) revert NotForSale();
         _;
     }
+
     modifier onlyOwner (uint256 _itemId){
-        require(msg.sender == ownerOf(_itemId));
+        if(msg.sender != ownerOf(_itemId)) revert NotOwner();
         _;
     }
     
     constructor() ERC721("Funoken", "FTK") {}
     
     function mintToken(string memory tokenURI) external returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        idToBid[newItemId] = TokenBid(false, 0, address(0), 0, 0, 0);
-        return newItemId;
+        unchecked {
+            ++_tokenId;
+        }
+        uint256 currentTokenId = _tokenId;
+
+        _mint(msg.sender, currentTokenId);
+        _setTokenURI(currentTokenId, tokenURI);
+
+        return currentTokenId;
     }
     
-    function auction(uint256 itemId, uint256 amount, uint8 active) external onlyOwner(itemId) {
-        require(idToBid[itemId].forSale == false);
-        idToBid[itemId].forSale = true;
-        idToBid[itemId].highestBid = amount;
-        idToBid[itemId].auctionStart = block.timestamp;
-        idToBid[itemId].activeTime = active;
-        emit Auction(itemId, ownerOf(itemId));
+    function auction(uint256 itemId, uint256 amount, uint256 activeTime) external onlyOwner(itemId) {
+        if(idToBid[itemId].forSale) revert AuctionActive();
+
+        idToBid[itemId] = TokenBid({
+            highestBid: amount,
+            auctionStart: block.timestamp,
+            bidsPlaced: 0,
+            activeTime: activeTime,
+            highestBidder: address(0),
+            forSale: true
+        });
+
+        emit Auction(itemId, msg.sender);
     }
     
     function bid(uint256 itemId) external payable upForSale(itemId){
-        require(block.timestamp<(idToBid[itemId].auctionStart + idToBid[itemId].activeTime*3600));
-        require(msg.value> idToBid[itemId].highestBid + 0.001 ether, "Bid must be higher"); 
-        require(msg.sender != ownerOf(itemId));
+        if(msg.sender == ownerOf(itemId)) revert OwnerNotAllowed();
+        if(tx.origin != msg.sender) revert ContractNotAllowed();
+    
+        TokenBid storage bid = idToBid[itemId];
+        if(block.timestamp > bid.auctionStart + bid.activeTime * 3600) revert AuctionTimeExpired();
+
+        if(msg.value <= bid.highestBid + 0.001 ether) revert BidTooLow();
         
-        if(idToBid[itemId].bidsPlaced>0){
-            payable(idToBid[itemId].highestBidder).transfer(idToBid[itemId].highestBid);
+        if(bid.bidsPlaced > 0)
+            bid.highestBidder.call{value: bid.highestBid}("");
+        
+
+        bid.highestBid = msg.value;
+        bid.highestBidder = msg.sender;
+        unchecked {
+            bid.bidsPlaced++;
         }
-        idToBid[itemId].highestBid = msg.value;
-        idToBid[itemId].highestBidder = msg.sender;
-        idToBid[itemId].bidsPlaced++;
+
         emit Bid(msg.sender, itemId, msg.value);
     }
     
     function endAuction(uint256 itemId) external upForSale(itemId) onlyOwner(itemId) {
-        require(block.timestamp > idToBid[itemId].auctionStart + idToBid[itemId].activeTime*3600, "Auction time has not ended yet");
-        if (idToBid[itemId].bidsPlaced>0){
-            payable(ownerOf(itemId)).transfer(idToBid[itemId].highestBid);
-            emit Transaction(itemId, ownerOf(itemId), idToBid[itemId].highestBidder, idToBid[itemId].highestBid);
-            transferFrom(ownerOf(itemId), idToBid[itemId].highestBidder, itemId); 
+        TokenBid memory bid = idToBid[itemId];
+        if(block.timestamp < bid.auctionStart + bid.activeTime * 3600) revert AuctionActive();
+
+        delete idToBid[itemId];
+
+        if (bid.bidsPlaced > 0){
+            address _owner = ownerOf(itemId);
+
+            _owner.call{value: bid.highestBid}("");
+            transferFrom(_owner, bid.highestBidder, itemId); 
+
+            emit Transaction(itemId, _owner, bid.highestBidder, bid.highestBid);
         }
-        idToBid[itemId].forSale = false;
-        idToBid[itemId].bidsPlaced = 0;
-        idToBid[itemId].highestBid = 0;
-        idToBid[itemId].highestBidder = address(0);    
-        idToBid[itemId].auctionStart = 0;
-        idToBid[itemId].activeTime = 0;
     }
 }
